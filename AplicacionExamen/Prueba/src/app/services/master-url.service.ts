c1dfc8ae64a558964cd7b0283836e9d23863ddc5
Tania Guamushig <tania.guamushig@epn.edu.ec>
2017-03-07 14:34:41 -0500

    Master URL
    cambio de la url que estaba en local host

--- AplicacionExamen/Prueba/src/app/services/master-url.service.ts
+++ AplicacionExamen/Prueba/src/app/services/master-url.service.ts
@@ -5,8 +5,8 @@ export class MasterURlService {
   private _url:string;
 
   constructor() {
-    this._url = "http://localhost:1337/";
-    // this.url = "https://aplicacion-adrianeguez.c9users.io"
+    //this._url = "http://localhost:1337/";
+    this.url = "https://examen-twj-guamushigtania-taniamaricela1993.c9users.io"
     // this.url = "https://asdasd-asdasdasd.herokuapp.com"
   }
 
